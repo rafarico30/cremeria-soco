8390a10925fec0e6b3a1043f633aef4e34f8dff9
Rafael Rico <rafarico08@gmail.com>
2025-02-06 12:18:31 -0600

    tickets para proveedores agregados

--- backend/routes/compras.js
+++ backend/routes/compras.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const Compra = require('../models/Compra');
 const Product = require('../models/Product');
+const Proveedor = require('../models/Proveedor');
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+const path = require('path');
+
+// Función para truncar texto
+const truncateText = (text, maxLength) => {
+  if (text.length > maxLength) {
+    return text.substring(0, maxLength) + '...';
+  }
+  return text;
+};
 
 // Ruta para crear una nueva compra (POST)
 router.post('/compras', [
@@ -31,10 +43,74 @@ router.post('/compras', [
     for (const item of productos) {
       const product = await Product.findById(item.producto);
       if (product) {
-        product.stock += item.cantidad; // Sumar la cantidad al stock existente
+        product.stock += item.cantidad;
         await product.save();
       }
     }
+
+    // Poblar los datos del proveedor y de los productos
+    const compraPopulada = await Compra.findById(compraGuardada._id)
+      .populate('proveedor')
+      .populate('productos.producto');
+
+    // Generar el ticket en formato PDF
+    const ticketPath = path.join(__dirname, 'tickets', 'compras', `compra_${compraGuardada._id}.pdf`);
+    const doc = new PDFDocument({ margin: 50 });
+    doc.pipe(fs.createWriteStream(ticketPath));
+
+    // Encabezado
+    doc.font('Times-Bold').fontSize(35).text('Cremería Soco', { align: 'center' });
+    doc.font('Times-Roman').fontSize(20).text('C. Nicolás Bravo 262, Centro histórico de Morelia', { align: 'center' });
+    doc.font('Times-Roman').fontSize(20).text('4431019999', { align: 'center' });
+    doc.moveDown();
+    doc.fontSize(20).text('---------------------------------------------------------------------', { align: 'center' });
+    doc.moveDown();
+    doc.fontSize(20).text('RECIBO PARA PROVEEDOR', { align: 'center' });
+    doc.moveDown();
+    doc.fontSize(20).text('---------------------------------------------------------------------', { align: 'center' });
+    doc.moveDown();
+    doc.fontSize(18).text(`Proveedor: ${compraPopulada.proveedor.nombreProveedor}`);
+    doc.text(`Fecha: ${fecha}`);
+    doc.moveDown();
+
+    // Tabla de productos
+    const tableTop = doc.y;
+    const descriptionX = 50;
+    const quantityX = 250;
+    const priceX = 350;
+    const totalX = 450;
+
+    doc.font('Times-Bold');
+    doc.text('Descripción', descriptionX, tableTop);
+    doc.text('Cantidad', quantityX, tableTop);
+    doc.text('Precio', priceX, tableTop);
+    doc.text('Total', totalX, tableTop);
+    doc.moveDown();
+    doc.font('Times-Roman');
+
+    compraPopulada.productos.forEach((item) => {
+      const y = doc.y;
+      const truncatedName = truncateText(item.producto.nombre, 15); // Truncar el nombre del producto a 15 caracteres
+      doc.text(truncatedName, descriptionX, y);
+      doc.text(item.cantidad.toString(), quantityX, y);
+      doc.text(`$${item.producto.precioProveedor.toFixed(2)}`, priceX, y);
+      doc.text(`$${(item.producto.precioProveedor * item.cantidad).toFixed(2)}`, totalX, y);
+      doc.moveDown();
+    });
+
+    doc.moveDown(2); // Baja un par de líneas para asegurarte de que no esté encima de la tabla
+    doc.text('', 50, doc.y); 
+
+    // Total de la compra
+    doc.fontSize(20).text('----------------------------------------------------------------------', { align: 'center' });
+    doc.moveDown();
+    doc.fontSize(20).text(`Total de la Compra: $${compraPopulada.productos.reduce((acc, item) => acc + (item.producto.precioProveedor * item.cantidad), 0).toFixed(2)}`, 50, doc.y, { align: 'left' });
+
+
+    doc.moveDown(2);
+    doc.font('Times-Bold').fontSize(20).text('¡Gracias por el producto!', 50, doc.y, { align: 'center' });
+
+    doc.end();
 
     res.status(201).json(compraGuardada);
   } catch (err) {
